5ea72fa7e335f01fa1ef9e799eb1e2c38abd7378
smarr2198 <smarr2198@gmail.com>
2022-02-25 13:35:58 -0600

    Team Profile Loop Update

--- main/createProfile.js
+++ main/createProfile.js
@@ -3,11 +3,13 @@ const createManagerProfile = function (manager) {
     <div class="card d-flex flex-wrap container bg-c mt-5" style="width: 18rem;">
         <div class="card-body bg-primary mx-0">
             <h5 class="card-title">${manager.name}</h5>
-            <h5 class="card-title mb-2">Manger</h5>
+            <h5 class="card-title mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-fill" viewBox="0 0 16 16">
+            <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8z"/>
+          </svg>   Manger</h5>
         </div>
         <ul class="list-group list-group-flush py-3">
             <li class="list-group-item w-100">ID: ${manager.id}</li>
-            <li class="list-group-item">Email: ${manager.email}</li>
+            <li class="list-group-item">Email: <a href = "mailto: ${manager.email}">${manager.email}</a></li>
             <li class="list-group-item">OfficeNumber: ${manager.officeNumber}</li>
         </ul>
     </div>
@@ -19,12 +21,14 @@ const createEngineerProfile = function (engineer) {
     <div class="card d-flex flex-wrap container bg-c mt-5" style="width: 18rem;">
         <div class="card-body bg-primary mx-0">
             <h5 class="card-title">${engineer.name}</h5>
-            <h5 class="card-title mb-2">Engineer</h5>
+            <h5 class="card-title mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+            <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+          </svg>   Engineer</h5>
         </div>
         <ul class="list-group list-group-flush py-3">
             <li class="list-group-item w-100">ID: ${engineer.id}</li>
-            <li class="list-group-item">Email: ${engineer.email}</li>
-            <li class="list-group-item">Github: ${engineer.github}</li>
+            <li class="list-group-item">Email: <a href = "mailto: ${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">Github <a href = "https://github.com/${engineer.github}">${engineer.github}</a></li>
         </ul>
     </div>
     `;
@@ -35,11 +39,14 @@ const createInternProfile = function (intern) {
     <div class="card d-flex flex-wrap container bg-c mt-5" style="width: 18rem;">
         <div class="card-body bg-primary mx-0">
             <h5 class="card-title">${intern.name}</h5>
-            <h5 class="card-title mb-2">Intern</h5>
+            <h5 class="card-title mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+            <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/>
+            <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
+          </svg>   Intern</h5>
         </div>
         <ul class="list-group list-group-flush py-3">
             <li class="list-group-item w-100">ID: ${intern.id}</li>
-            <li class="list-group-item">Email: ${intern.email}</li>
+            <li class="list-group-item">Email: <a href = "mailto: ${intern.email}">${intern.email}</a></li>
             <li class="list-group-item">University: ${intern.university}</li>
         </ul>
     </div>
